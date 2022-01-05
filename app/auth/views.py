6f8f4f3efbfed41e560e4a5392ffb459efedfb84
Sebastian Diaz <sebastiandiazmartinez@gmail.com>
2022-01-04 20:51:43 -0500

    commit1

---
+++ app/auth/views.py
@@ -0,0 +1,78 @@
+from flask import render_template,session,flash,redirect,url_for
+from flask_login import login_user, login_required, logout_user
+from werkzeug.security import generate_password_hash
+from app.forms import LoginForm
+from . import auth
+from app.firestone_service import get_user,put_user
+from app.models import UserData,UserModel
+
+@auth.route('/login', methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    context = {
+        'login_form': login_form
+    }
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        password = login_form.password.data
+
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is not None:
+            password_from_db = user_doc.to_dict()['password']
+
+            if password == password_from_db:
+                user_data = UserData(username,password)
+                user = UserModel(user_data)
+                
+                login_user(user)
+                flash('Bienvenido de nuevo')
+
+                redirect(url_for('hello'))   
+            else:
+                flash('Password incorrecto')
+        else:
+            flash('Usuario no existe')
+
+
+        
+        return redirect(url_for('index'))
+
+    return render_template('login.html', **context)
+
+@auth.route('signup', methods=['GET','POST'])
+def signup():
+    signup_form = LoginForm()
+    context = {
+        'signup_form' : signup_form
+        
+    }
+    
+    if signup_form.validate_on_submit():
+        username =  signup_form.username.data
+        password = signup_form.password.data
+
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is None:
+            password_hash = generate_password_hash(password)
+            user_data = UserData(username,  password_hash)
+            put_user(user_data)
+
+            flash('El usuario registrado exitosamente')
+            user = UserModel(user_data)
+            login_user(user)
+            flash('Sesión iniciada')
+            
+        else:
+            flash('El usuario ya esta registrado')
+
+    return render_template('signup.html', **context)
+
+@auth.route('logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Regresa pronto')
+    return redirect(url_for('auth.login'))
+
---
+++ app/forms.py
@@ -0,0 +1,19 @@
+from firebase_admin.firestore import client
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()] )
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
+
+class TodoForm(FlaskForm):
+    description = StringField('Descripción', validators=[DataRequired()])
+    submit = SubmitField('Crear')
+
+class DeleteToDoForm(FlaskForm):
+    submit = SubmitField('Borrar')
+
+class UpdateToDoForm(FlaskForm):
+    submit = SubmitField('Actualizar')
